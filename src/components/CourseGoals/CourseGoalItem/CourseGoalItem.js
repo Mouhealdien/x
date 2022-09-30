59cd59d25120c86d34a77be4b9bbfd08b49df24c
Mouhealdien <81492994+Mouhealdien@users.noreply.github.com>
2022-09-30 05:59:20 +0300

    Add files via upload

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './CourseGoalItem.css';
+
+const CourseGoalItem = props => {
+  // const [deleteText, setDeleteText] = useState('');
+
+  const deleteHandler = () => {
+    // setDeleteText('(Deleted!)');
+    console.log(props)
+    console.log(props.id);
+    props.onDelete(props.id);
+    
+  };
+
+  return (
+    <li className="goal-item" onClick={deleteHandler}>
+      {props.children}
+    </li>
+  );
+};
+
+export default CourseGoalItem;
